fc2a8b21bfff67816914ab35fe9be964442cd3ce
GbertonAR <gberton1967@gmail.com>
2025-04-16 11:45:54 +0000

    Instalar tesseract-ocr

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,29 @@
 #!/bin/bash
-set -e
+set -euo pipefail
+sudo apt update
+sudo add-apt-repository universe -y
+sudo apt update
+sudo apt install -y tesseract-ocr
+sudo apt install -y tesseract-ocr-eng
 
-echo "Ejecutando script de despliegue personalizado..."
+# #!/bin/bash
+# set -e
 
-# Actualizar los repositorios
-echo "Actualizando repositorios..."
-sudo apt-get update
+# echo "Ejecutando script de despliegue personalizado..."
 
-# Instalar Tesseract OCR y paquetes de idioma español
-echo "Instalando Tesseract OCR y paquete de idioma español..."
-sudo apt-get -y install tesseract-ocr tesseract-ocr-spa
+# # Actualizar los repositorios
+# echo "Actualizando repositorios..."
+# sudo apt-get update
 
-echo "Tesseract OCR instalado."
+# # Instalar Tesseract OCR y paquetes de idioma español
+# # echo "Instalando Tesseract OCR y paquete de idioma español..."
+# # sudo apt-get -y install tesseract-ocr tesseract-ocr-spa
 
-# Puedes agregar aquí cualquier otro comando que necesites ejecutar
-# DESPUÉS de que Oryx haya instalado las dependencias de Python.
 
-echo "Script de despliegue personalizado completado."
+
+# echo "Tesseract OCR instalado."
+
+# # Puedes agregar aquí cualquier otro comando que necesites ejecutar
+# # DESPUÉS de que Oryx haya instalado las dependencias de Python.
+
+# echo "Script de despliegue personalizado completado."
