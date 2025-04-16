76a1bc0a486a7986b29332947b982c2d3d7cc518
GbertonAR <gberton1967@gmail.com>
2025-04-16 00:36:20 +0000

    Instalar tesseract-ocr

--- deploy.sh
+++ deploy.sh
@@ -13,15 +13,7 @@ sudo apt-get -y install tesseract-ocr tesseract-ocr-spa
 
 echo "Tesseract OCR instalado."
 
-# Instalar dependencias de Python desde requirements.txt
-echo "Instalando dependencias de Python..."
-if [ -f "$HOME/site/wwwroot/requirements.txt" ]; then
-    python -m pip install -r "$HOME/site/wwwroot/requirements.txt" --no-cache-dir
-fi
-
-echo "Dependencias de Python instaladas."
-
 # Puedes agregar aquí cualquier otro comando que necesites ejecutar
-# después de la instalación de Tesseract y las dependencias.
+# DESPUÉS de que Oryx haya instalado las dependencias de Python.
 
 echo "Script de despliegue personalizado completado."
